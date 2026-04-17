6e403be7144776f3f60fe3985cf44ddb0bd555d8
daviddemri26 <david.demri@me.com>
2026-04-17 15:21:47 -0700

    Initial Commit

---
+++ SweetKeyboard/SweetKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetKeyboardApp.swift
+//  SweetKeyboard
+//
+//  Created by David Demri on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SweetKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
